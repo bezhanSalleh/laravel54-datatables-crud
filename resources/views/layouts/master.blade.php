238945c45959acc3d8acfc37be78093dce201a91
Bezhan Salleh <bezhan_salleh@yahoo.com>
2019-01-13 21:58:53 +0430

    bootstrap 4

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -7,12 +7,10 @@
         <meta name="csrf-token" content="{{csrf_token()}}">
         <title>Ajax Crud</title>
         <!--bootstrap cdn-->
-        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4-4.1.1/jq-3.3.1/dt-1.10.18/r-2.2.2/rr-1.2.4/sc-1.5.0/sl-1.2.6/datatables.min.css"/>
+
         <!-- sweetalert-->
         <link rel="stylesheet" type="text/css" href="{{asset('sweetalert/sweetalert.css')}}">
-        <!-- datatables cdn-->
-        {{-- <link href="https://cdn.datatables.net/1.10.15/css/jquery.dataTables.min.css" rel="stylesheet"> --}}
-        <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs/jszip-2.5.0/dt-1.10.18/b-1.5.4/b-flash-1.5.4/b-html5-1.5.4/b-print-1.5.4/datatables.min.css"/>
 
 
     </head>
@@ -24,18 +22,8 @@
         
 
         <!--start of scripts-->
-        <!--jquery-->
-        <script src="https://code.jquery.com/jquery-1.12.4.js" type="text/javascript"></script>
-        <!-- datatables cdn-->
-        {{-- <script src="https://cdn.datatables.net/1.10.15/js/jquery.dataTables.min.js" type="text/javascript"></script> --}}
-        <!--bootstrap cdn-->
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-        <!-- sweetalert-->
+        <script type="text/javascript" src="https://cdn.datatables.net/v/bs4-4.1.1/jq-3.3.1/dt-1.10.18/r-2.2.2/rr-1.2.4/sc-1.5.0/sl-1.2.6/datatables.min.js"></script>
         <script src="{{asset('sweetalert/sweetalert.min.js')}}" type="text/javascript" charset="utf-8" async defer></script>
-
-        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
-        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
-        <script type="text/javascript" src="https://cdn.datatables.net/v/bs/jszip-2.5.0/dt-1.10.18/b-1.5.4/b-flash-1.5.4/b-html5-1.5.4/b-print-1.5.4/datatables.min.js"></script>
         @stack('scripts')
         <!---end of scripts-->
     </body>
